8228c2c1ba1b11c61d2318361758c4ea28e155f7
lindakwoo <lindakwoo@gmail.com>
2024-10-11 16:01:34 -0700

    test with exclamation

--- client/src/components/SendMessage.jsx
+++ client/src/components/SendMessage.jsx
@@ -62,7 +62,7 @@ const SendMessage = () => {
         {patient ? (
           <>
             <Typography variant='h6' gutterBottom>
-              Send a Text Message to: {patient.first_initial}.{patient.last_initial}.
+              Send a Text Message to {patient.first_initial}.{patient.last_initial}.
             </Typography>
             <TextField
               fullWidth
@@ -92,7 +92,7 @@ const SendMessage = () => {
               sx={{ mt: 2 }}
               fullWidth
             >
-              {isSending ? "Sending..." : "Send Message"}
+              {isSending ? "Sending..." : "Send Message!"}
             </Button>
           </>
         ) : (
