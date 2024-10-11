d5369f97d4820fa5b65a916326d1cb64364303c8
lindakwoo <lindakwoo@gmail.com>
2024-10-11 11:44:23 -0700

    move firebase json

--- clinic/settings.py
+++ clinic/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x-vohcdfaodbz!!ket=gy9@^dxu@3h^!fz$(*nj-v^=+9#33vm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
